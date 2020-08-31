88681b70a7417ae9be57199a40ac501d9ac77a2f
Fernanda Diomede <fcdiomede@gmail.com>
2020-08-30 22:13:28 -0500

    Add edit cookbook button

--- src/cookbook.jsx
+++ src/cookbook.jsx
@@ -3,25 +3,36 @@ import { useHistory } from "react-router-dom";
 
 function CookbookCover(props) {
 
-    let history = useHistory()
+    let history = useHistory();
 
-    const data = {'cookbookId': props.cookbookId}
+    const data = { 'cookbookId': props.cookbookId };
 
     const goToCookbook = () => {
-        fetch('/api/set-cookbook', {method: 'POST', 
-        body: JSON.stringify(data), 
-        headers: {'Accept': 'application/json',
-        'Content-Type': 'application/json'}})
-        .then(() => history.push('/recipes'));
-    }
+        fetch('/api/set-cookbook', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(() => history.push('/recipes'));
+    };
+
+    const editCover = (evt) => {
+
+    };
 
     return (
-        <div className="cookbook" onClick={goToCookbook}>
-          <p>Name: {props.title}</p>
-          <img src={props.imgUrl} />
-        </div>
-      );
-    }
+        <React.Fragment>
+            <div className="cookbook" onClick={goToCookbook}>
+                <p>Name: {props.title}</p>
+                <img src={props.imgUrl} />
+            </div>
+            <button onClick={editCover}>Edit Cookbook Cover</button>
+        </React.Fragment>
+    );
+}
 
 
 //component to hold all cookbook cards
@@ -33,26 +44,26 @@ export function CookbookContainer(props) {
     const [cookbooks, updateCookbooks] = React.useState([]);
 
     React.useEffect(() => {
-      fetch('/api/user-cookbooks')
-      .then((res) => res.json())
-      .then((data) => updateCookbooks(data))
+        fetch('/api/user-cookbooks')
+            .then((res) => res.json())
+            .then((data) => updateCookbooks(data));
     }, [props.showCookbookCreation]);
 
-    const userCookbooks = []
+    const userCookbooks = [];
     for (const cookbook of cookbooks) {
         userCookbooks.push(
-          <CookbookCover
-            key={cookbook.key}
-            cookbookId={cookbook.key}
-            title={cookbook.title}
-            imgUrl={cookbook.imgUrl}
-          />
+            <CookbookCover
+                key={cookbook.key}
+                cookbookId={cookbook.key}
+                title={cookbook.title}
+                imgUrl={cookbook.imgUrl}
+            />
         );
-      }
-     
-      return (
+    }
+
+    return (
         <div>{userCookbooks}</div>
-      );
+    );
 }
 
 export function CreateNewCookbook(props) {
@@ -60,12 +71,12 @@ export function CreateNewCookbook(props) {
     // let history = useHistory();
 
     const goToCreateCookbookForm = () => {
-        props.setShowCookbookCreation(true)
-    }
+        props.setShowCookbookCreation(true);
+    };
 
     return (
         <button onClick={goToCreateCookbookForm}>Create New Cookbook</button>
-    )
+    );
 }
 
 export function NewCookbookForm(props) {
@@ -75,51 +86,56 @@ export function NewCookbookForm(props) {
     const [photo, setPhoto] = React.useState('');
 
     //cloudinary config
-    const uploadWidget = window.cloudinary.createUploadWidget({ 
-        cloudName: "deglaze", uploadPreset: "cuca-preset" }, (error, result) => {
-            if (result.event === 'success') {
-                setPhoto(result.info.url)
-            }
-         });
+    const uploadWidget = window.cloudinary.createUploadWidget({
+        cloudName: "deglaze", uploadPreset: "cuca-preset"
+    }, (error, result) => {
+        if (result.event === 'success') {
+            setPhoto(result.info.url);
+        }
+    });
 
     //callback after creating cookbook
     const saveCookbook = (evt) => {
         evt.preventDefault();
-        props.setShowCookbookCreation(false)
-        
+        props.setShowCookbookCreation(false);
+
         const data = {
             'title': title,
             'photo': photo
-        }
+        };
 
-        fetch('/api/new-cookbook', {method: 'POST',
-        body: JSON.stringify(data),
-        headers: {'Accept': 'application/json',
-        'Content-Type': 'application/json'}})
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data)
+        fetch('/api/new-cookbook', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
         })
-    }
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            });
+    };
 
     const cancel = (evt) => {
         evt.preventDefault();
-        props.setShowCookbookCreation(false)
-    }
+        props.setShowCookbookCreation(false);
+    };
 
     //newcookbook form
     return (
         <form>
             <label>Title</label>
-             <input type='text'
-                     id='title'
-                     onChange={(evt) => setTitle(evt.target.value)}
-                     value={title}></input>
-             <label>Cover Photo</label>
+            <input type='text'
+                id='title'
+                onChange={(evt) => setTitle(evt.target.value)}
+                value={title}></input>
+            <label>Cover Photo</label>
             <input type='button' onClick={uploadWidget.open} value='Add Cover Image' />
             <img src={photo}></img>
             <button onClick={saveCookbook}>Create It!</button>
             <button onClick={cancel}>Cancel</button>
         </form>
-    )
- }
+    );
+}
